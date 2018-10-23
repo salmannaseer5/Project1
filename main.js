bcefec017375c4dee150c3ef3c4630e5bca559bb
MJ Sandoval <salssedan@gmail.com>
2018-10-23 14:17:14 -0400

    JS arrays

--- main.js
+++ main.js
@@ -1,1 +1,103 @@
 console.log("Welcome to click on Play to start");
+const geographyQuestion = [
+  {
+    question: "What is the capital of United States of America?",
+    answers: {
+      a: "New York",
+      b: "Chicago",
+      c: "Washington DC",
+      d: "Boston"
+    },
+    correctAnswer: "c"
+  },
+
+  {
+    question: "What are motorways called in France?",
+    answers: {
+      a: "Autoroutes",
+      b: "AutoBahn",
+      c: "Autostrada",
+      d: "AutoPista"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    question: "Which of these is the deepest?",
+    answers: {
+      a: "The Mponeng gold mine",
+      b: "Lake Baikal",
+      c: "Mariana Trench",
+      d: "Krubera cave"
+    },
+    correctAnswer: "c"
+  },
+
+  {
+    question: "Which tiny island country did Columbus discover on a Sunday?",
+    answers: {
+      a: "Dominica",
+      b: "Montserrat",
+      c: "Cuba",
+      d: "Antigua"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    question:
+      "What North American mountain range is an apt anagram for 'o man, ski country'?",
+    answers: {
+      a: "Adirondack",
+      b: "Cascades",
+      c: "Rocky Mountains",
+      d: "Appalachians"
+    },
+    correctAnswer: "c"
+  },
+
+  {
+    question: "What is the highest point of Switzerland?",
+    answers: {
+      a: "Mont Blanc",
+      b: "Zumsteinsptize",
+      c: "Matterhorn",
+      d: "Dufoursptize"
+    },
+    correctAnswer: "d"
+  },
+
+  {
+    question: "Where is Romania?",
+    answers: {
+      a: "Europe",
+      b: "Asia",
+      c: "Africa",
+      d: "South America"
+    },
+    correctAnswer: "a"
+  },
+
+  {
+    question: "What is the Capital of Canada?",
+    answers: {
+      a: "Vancouver",
+      b: "Toronto",
+      c: "Ottawa",
+      d: "Montreal"
+    },
+    correctAnswer: "b"
+  },
+
+  {
+    question:
+      "I'm standing in Red Square admiring the St. Basil's Cathedral in Europe's most populated city. Where am I ?",
+    answers: {
+      a: "Moscow",
+      b: "Rome",
+      c: "Cairo",
+      d: "Paris"
+    },
+    correctAnswer: "a"
+  }
+];
